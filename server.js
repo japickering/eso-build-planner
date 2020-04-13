515222460635a4f202d176252c244be8cb62d7a4
James Pickering <james.pickering@gmail.com>
2020-04-13 19:33:49 +0100

    Deployment to Heroku

--- server.js
+++ server.js
@@ -43,4 +43,14 @@ app.all("/secret", (req, res, next) => {
   next(); // pass control to the next handler
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+    
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 app.listen(port, () => console.log(`Server listening on port ${port}`));
